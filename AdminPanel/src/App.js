67aac91a556c188ade640823d4e3035889848290
Lynch <awstyler@3c22fb083573.ant.amazon.com>
2020-07-24 13:56:12 -0400

    added auth and auhub

--- AdminPanel/src/App.js
+++ AdminPanel/src/App.js
@@ -7,7 +7,7 @@ import logo from './oktanklogo.png';
 import './App.css';
 import JsonTable from 'ts-react-json-table';
 import Popup from 'react-popup';
-import Amplify, { API, graphqlOperation, formRow } from "aws-amplify";
+import Amplify, { API, graphqlOperation, Auth, Hub } from "aws-amplify";
 
 Amplify.configure(aws_exports);
 
@@ -17,7 +17,8 @@ class ChatContent extends React.Component{
 		this.state = {
 			chatMessages: [],
 			inputMessage: '',
-			course: 3
+			username: '',
+			course: 3		
 		};
 
 		this.handleInputChange = this.handleInputChange.bind(this)
@@ -57,6 +58,7 @@ class ChatContent extends React.Component{
 	componentDidMount(){
 		this.listenForChatMessages();
 		this.loadChatMessages();
+		this.setState({username: this.props.username});
 	}
 
 	loadChatMessages = () => {
@@ -113,7 +115,7 @@ class ChatContent extends React.Component{
 
 			const message = {
 				input: {
-					username: 'tlynch',
+					username: this.state.username,
 					message: this.state.inputMessage,
 					course: this.state.course
 				}
@@ -161,16 +163,16 @@ class VideoContent extends React.Component {
 
 	handleSuccess = (stream) => {
 		//debugger;
-		const video = document.getElementById('localVideo');
 		const videoTracks = stream.getVideoTracks();
 		console.log('Got stream with constraints:', this.constraints);
 		console.log(`Using video device: ${videoTracks[0].label}`);
 		window.stream = stream; // make variable available to browser console
-		video.srcObject = stream;
+		this.player.srcObject = stream;
 	  }
 
 	showVideo = (event) => {
-
+		const foo = 0 ;
+		
 		navigator.mediaDevices.getUserMedia(this.constraints).
 			then ((stream) => {
 				this.handleSuccess(stream);
@@ -189,8 +191,7 @@ class VideoContent extends React.Component {
 				}
 			});
 
-			const video = document.getElementById('localVideo');
-			video.srcObject = null;
+			this.player.srcObject = null;
 		}
 	}
 
@@ -201,7 +202,7 @@ class VideoContent extends React.Component {
 	render() {
 		return (
 			<div>
-				<div><video name="localVideo" id="localVideo" autoPlay playsInline ></video></div>
+				<div><video  ref={(player) => this.player = player} autoPlay playsInline ></video></div>
 				<div><button name="showVideo" onClick={this.showVideo}>Open Camera</button><button  name="hideVideo" onClick={this.hideVideo}>Close Camera</button></div>
 			</div>
 		);
@@ -209,19 +210,77 @@ class VideoContent extends React.Component {
 }
 
 class App extends Component {
-	render() {
-		return (
+	constructor(props){
+		super(props);
+		this.state = {
+			username: "",
+			user: "",
+			customState: null,
+		};
+	}
+
+	
+	componentDidMount(){
+		//this.listenForQuestions();
+
+		Hub.listen("auth", ({ payload: { event, data } }) => {
+			switch (event) {
+			case "signIn":
+			  this.setState({ user: data });
+			  this.setState({ username: data.signInUserSession.idToken.payload.email });
+			  break;
+			case "signOut":
+			  this.setState({ user: null });
+			  break;
+			case "customOAuthState":
+			  this.setState({ customState: data });
+		  }
+		});
+	
+		Auth.currentAuthenticatedUser()
+		  .then(user => this.setState({ user }))
+		  .catch(() => console.log("Not signed in"));
+	}
+	
+
+	askForName = () => {
+		return(
 			<div className="App">
-				<Popup />
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Instructor LIVE!</h1>
+					<h1 className="App-title">Instructor LIVE! - Admin</h1>
 				</header>
-				<br />
-				<VideoContent />
-				<ChatContent />
+				<div className="">
+					<center>
+						<div className="username-prompt">
+							<div className="username-prompt-input-container">
+							<button onClick={() => Auth.federatedSignIn({provider: 'instructorlive-admin'})}>Login with Okta</button>
+							</div>
+						</div>
+					</center>
+				</div>
 			</div>
 		);
+	}
+
+
+	render() {
+		if(this.state.username == ""){
+			return this.askForName();	
+		} else {		
+			return (
+				<div className="App">
+					<Popup />
+					<header className="App-header">
+						<img src={logo} className="App-logo" alt="logo" />
+						<h1 className="App-title">Instructor LIVE! {this.state.username}</h1>
+					</header>
+					<br />
+					<VideoContent />
+					<ChatContent  username={ this.state.username } />
+				</div>
+			);
+		}
 	}
 }
 
